dc22c7df13fd0ea211702894c4742369338d54c5
wslulciuc <willy@canary.is>
2017-03-08 12:35:12 -0500

    Add event collection endpoint

--- src/jvm/mockingbird/Api.scala
+++ src/jvm/mockingbird/Api.scala
@@ -1,11 +1,18 @@
 package mockingbird.api
 
-import io.finch._
 import com.twitter.finagle.Http
 import com.twitter.util.Await
+import io.finch._
+import io.finch.circe._
+import mockingbird._
 
 object Api extends App {
-  val api = get("sing") { Ok("chirp") }
-  val server = Http.server.serve(":8080", api.toServiceAs[Text.Plain])
+  val postEvents: Endpoint[String] = post("events") { Ok("chirp") }
+  val postEvent: Endpoint[String] = post("events" :: string) {
+    collection: String =>
+      Ok("chirp")
+  }
+  val api = (postEvents :+: postEvent).toServiceAs[Application.Json]
+  val server = Http.server.serve(":8080", api)
   Await.ready(server)
 }
