4410fae68102dcd69d9d51ac657a7dcbc41feae9
wslulciuc <willy@canary.is>
2017-03-09 14:37:52 -0500

    continued: Add event collection endpoint

--- src/jvm/mockingbird/Api.scala
+++ src/jvm/mockingbird/Api.scala
@@ -5,13 +5,23 @@ import com.twitter.util.Await
 import io.finch._
 import io.finch.circe._
 import mockingbird._
+import mockingbird.Request._
 
 object Api extends App {
   val postEvents: Endpoint[String] = post("events") { Ok("chirp") }
-  val postEvent: Endpoint[String] = post("events" :: string) {
-    collection: String =>
-      Ok("chirp")
-  }
+
+  val postEvent: Endpoint[String] =
+    post("events" :: string :: jsonBody[Request]) {
+      (collectionName: String, req: Request) =>
+        EventCollection(collectionName) match {
+          case Some(eventCollection) => Ok("chirp")
+          case None =>
+            NotFound(
+                new Exception(
+                    s"The collection $collectionName does not exists."))
+        }
+    }
+
   val api = (postEvents :+: postEvent).toServiceAs[Application.Json]
   val server = Http.server.serve(":8080", api)
   Await.ready(server)
--- src/jvm/mockingbird/collections.scala
+++ src/jvm/mockingbird/collections.scala
@@ -1,4 +1,29 @@
 package mockingbird
 
-sealed trait EventCollection
-object SensorCollection extends EventCollection
+import org.apache.avro.Schema
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+sealed trait EventCollection {
+  type S <: Schema
+  val schema: S
+  val name: String
+
+  def addEvents(events: Seq[Event])(
+      implicit ec: ExecutionContext): Future[Seq[Event]] =
+    Future {
+      ???
+    }
+}
+object EventCollection {
+  def apply(name: String): Option[EventCollection] = name match {
+    case SensorCollection.name => Some(SensorCollection)
+    case _ => None
+  }
+}
+
+object SensorCollection extends EventCollection {
+  type S = Schema
+  val schema = ???
+  val name = "sensor"
+}
